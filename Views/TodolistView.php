2606e6d7043d3c846fb478907b07a44d65496bc5
Hadi Rizaldi <hadirizaldi.hr@gmail.com>
2022-09-08 01:51:33 +0700

    Make View

---
+++ Views/TodolistView.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace View;
+
+class TodolistView
+{
+    function showTodolist(): void
+    {
+    }
+
+    function addTodoliss(): void
+    {
+    }
+
+    function removeTodolist(): void
+    {
+    }
+}
